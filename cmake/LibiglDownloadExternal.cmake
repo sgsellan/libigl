275e84d898ef21eb0bf08365a8e1f1c6d776e9db
Silvia Sellán <sgsellan@gmail.com>
2025-10-05 08:53:40 -0400

    Update LibiglDownloadExternal.cmake

--- cmake/LibiglDownloadExternal.cmake
+++ cmake/LibiglDownloadExternal.cmake
@@ -86,7 +86,7 @@ endfunction()
 function(igl_download_glad)
 	igl_download_project(glad
 		GIT_REPOSITORY https://github.com/libigl/libigl-glad.git
-		GIT_TAG        09b4969c56779f7ddf8e6176ec1873184aec890f
+		GIT_TAG        https://github.com/libigl/libigl-glad.git
 	)
 endfunction()
 
